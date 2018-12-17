4e9f6745caf71e263c0c67396b7f4f5e41857fc3
unknown <user@DESKTOP-515UCQ5.cnlab.com>
2018-12-17 11:33:45 -0800

    Initial commit

---
+++ app/src/main/java/com/example/user/multiplebutton/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.user.multiplebutton;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button comillaButton, lalmoyButton, moynamotiButton, bardButton;
+    private Intent intent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        comillaButton = (Button) findViewById(R.id.comillaButtonId);
+        bardButton = (Button) findViewById(R.id.bardButtonId);
+        moynamotiButton = (Button) findViewById(R.id.moynamotiButtonId);
+
+        lalmoyButton = (Button) findViewById(R.id.lalmoyButtonId);
+        comillaButton.setOnClickListener(this);
+        bardButton.setOnClickListener(this);
+        moynamotiButton.setOnClickListener(this);
+        lalmoyButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.comillaButtonId) {
+            intent = new Intent(MainActivity.this, secondActivity.class);
+            intent.putExtra("name", "comilla");
+            startActivity(intent);
+
+        }
+        if (v.getId() == R.id.bardButtonId) {
+            intent = new Intent(MainActivity.this, secondActivity.class);
+            intent.putExtra("name", "bard");
+            startActivity(intent);
+
+        }
+        if (v.getId() == R.id.moynamotiButtonId) {
+            intent = new Intent(MainActivity.this, secondActivity.class);
+            intent.putExtra("name", "moynamoti");
+            startActivity(intent);
+
+        }
+        if (v.getId() == R.id.lalmoyButtonId) {
+            intent = new Intent(MainActivity.this, secondActivity.class);
+            intent.putExtra("name", "lalmoy");
+            startActivity(intent);
+
+        }
+
+    }
+}
